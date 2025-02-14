0476e20c4239020e9957de0fb8c0dd252225d964
isaacrisi <isaacpinedamunera@gmail.com>
2025-02-13 21:11:30 -0500

    corrections

--- Assets/VolverAlMenu.cs
+++ Assets/VolverAlMenu.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BOTONVOLVER : MonoBehaviour
+public class VolverAlMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     public void VolverMenu()
