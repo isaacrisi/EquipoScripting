0976e17a49fb8da57356b6c7b851f682f0e95d26
NicolasQ455359 <nicolas.quintero@upb.edu.co>
2025-03-05 10:02:06 -0500

    Create Coin.cs
    Se agrego el script para las monedas con giros constantemente

---
+++ Assets/Juego/SCRIPTS/Coin.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public float rotationSpeed = 100f; // Velocidad de rotacion
+
+    private void Update()
+    {
+        
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime); // Gira la moneda alrededor del eje Y
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // el jugador debe de tener el tag "Player"
+        {
+            Destroy(gameObject); // Destruye la moneda al tocarla
+        }
+    }
+}
