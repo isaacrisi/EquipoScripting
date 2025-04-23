fcfe8e863e726e7c577765167a2d09dd65a6d401
isaacrisi <isaacpinedamunera@gmail.com>
2025-04-23 10:39:10 -0500

    enemies

--- Assets/Juego/SCRIPTS/AIActionSelfDestruct.cs
+++ Assets/Juego/SCRIPTS/AIActionSelfDestruct.cs
@@ -1,22 +1,25 @@
-﻿using MoreMountains.Tools;
+using MoreMountains.Tools;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace MoreMountains.CorgiEngine
 {
+    /// <summary>
+    /// An AIACtion used to have an AI kill itself
+    /// </summary>
     [AddComponentMenu("Corgi Engine/Character/AI/Actions/AIActionSelfDestruct")]
     public class AIActionSelfDestruct : AIAction
     {
         public bool OnlyRunOnce = true;
-        public float ExplosionRadius = 2f;
-        public int DamageToPlayer = 20;
-        public LayerMask PlayerLayer;
 
         protected Character _character;
         protected Health _health;
         protected bool _alreadyRan = false;
 
+        /// <summary>
+        /// On init we grab our Health
+        /// </summary>
         public override void Initialization()
         {
             if (!ShouldInitialize) return;
@@ -25,41 +28,27 @@ namespace MoreMountains.CorgiEngine
             _health = _character.CharacterHealth;
         }
 
+        /// <summary>
+        /// Kills the AI
+        /// </summary>
         public override void PerformAction()
         {
             if (OnlyRunOnce && _alreadyRan)
             {
                 return;
             }
-
-            // Aplicar daño en área
-            Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, ExplosionRadius, PlayerLayer);
-            foreach (Collider2D hit in hits)
-            {
-                Health playerHealth = hit.GetComponent<Health>();
-                if (playerHealth != null)
-                {
-                    playerHealth.Damage(DamageToPlayer, gameObject, 0f, 0f, Vector2.zero);
-                }
-            }
-
-            // Autodestrucción
             _health.Kill();
             _brain.BrainActive = false;
             _alreadyRan = true;
         }
 
+        /// <summary>
+        /// On enter state we reset our flag
+        /// </summary>
         public override void OnEnterState()
         {
             base.OnEnterState();
             _alreadyRan = false;
-        }
-
-        // Solo para visualización en el editor
-        private void OnDrawGizmosSelected()
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, ExplosionRadius);
         }
     }
 }
