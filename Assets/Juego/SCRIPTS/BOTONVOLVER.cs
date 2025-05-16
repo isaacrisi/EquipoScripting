0c16590abc78f533ddd69c5b2d68f127b794b033
Judiego <168317943+Judiego@users.noreply.github.com>
2025-05-16 11:48:40 -0500

    Update BOTONVOLVER.cs

--- Assets/Juego/SCRIPTS/BOTONVOLVER.cs
+++ Assets/Juego/SCRIPTS/BOTONVOLVER.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class BOTONVOLVER : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void VolverMenu()
+    public void VolverMenu(
     {
         MMSceneLoadingManager.LoadScene("MENU");
     }
